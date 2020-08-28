cb1b01fe2ba1f5f70e27cd8c3ded8eec98723fd2
Fernand Pajot <fernand@epigram.me>
2020-08-27 19:47:15 -0600

    Initial commit.

---
+++ data.py
@@ -0,0 +1,45 @@
+import torch
+import torchaudio
+import torch.nn as nn
+
+from text import TextTransform
+
+train_audio_transforms = nn.Sequential(
+    torchaudio.transforms.MelSpectrogram(
+        sample_rate=16000, n_fft=400, hop_length=160, n_mels=80, power=1.0
+    ),
+    torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
+    torchaudio.transforms.TimeMasking(time_mask_param=35),
+)
+
+valid_audio_transforms = torchaudio.transforms.MelSpectrogram(
+    sample_rate=16000, n_fft=400, hop_length=160, n_mels=80, power=1.0
+)
+
+text_transform = TextTransform()
+
+
+def data_processing(data, data_type="train"):
+    spectrograms = []
+    labels = []
+    input_lengths = []
+    label_lengths = []
+    for (waveform, _, utterance, _, _, _) in data:
+        if data_type == "train":
+            spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        else:
+            spec = valid_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        spectrograms.append(spec)
+        label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
+        labels.append(label)
+        input_lengths.append(spec.shape[0] // 2)
+        label_lengths.append(len(label))
+
+    spectrograms = (
+        nn.utils.rnn.pad_sequence(spectrograms, batch_first=True)
+        .unsqueeze(1)
+        .transpose(2, 3)
+    )
+    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+
+    return spectrograms, labels, input_lengths, label_lengths
---
+++ train.py
@@ -0,0 +1,212 @@
+from comet_ml import Experiment
+import torch
+import torchaudio
+import torch.nn as nn
+import torch.nn.functional as F
+
+import words
+import net
+import data
+
+
+class IterMeter(object):
+    """keeps track of total iterations"""
+
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
+
+
+def train(
+    model, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment,
+):
+    model.train()
+    data_len = len(train_loader.dataset)
+    with experiment.train():
+        for batch_idx, _data in enumerate(train_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.cuda(), labels.cuda()
+
+            optimizer.zero_grad()
+
+            output = model(spectrograms)  # (batch, time, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)  # (time, batch, n_class)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+            loss.backward()
+
+            experiment.log_metric("loss", loss.item(), step=iter_meter.get())
+            experiment.log_metric(
+                "learning_rate", scheduler.get_lr(), step=iter_meter.get()
+            )
+
+            optimizer.step()
+            scheduler.step()
+            iter_meter.step()
+            if batch_idx % 100 == 0 or batch_idx == data_len:
+                print(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(spectrograms),
+                        data_len,
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
+
+
+def test(model, test_loader, criterion, epoch, iter_meter, experiment):
+    print("\nevaluating…")
+    model.eval()
+    test_loss = 0
+    test_cer, test_wer = [], []
+    with experiment.test():
+        with torch.no_grad():
+            for I, _data in enumerate(test_loader):
+                spectrograms, labels, input_lengths, label_lengths = _data
+                spectrograms, labels = spectrograms.cuda(), labels.cuda()
+
+                output = model(spectrograms)  # (batch, time, n_class)
+                output = F.log_softmax(output, dim=2)
+                output = output.transpose(0, 1)  # (time, batch, n_class)
+
+                loss = criterion(output, labels, input_lengths, label_lengths)
+                test_loss += loss.item() / len(test_loader)
+
+                decoded_preds, decoded_targets = GreedyDecoder(
+                    output.transpose(0, 1), labels, label_lengths
+                )
+                for j in range(len(decoded_preds)):
+                    test_cer.append(words.cer(decoded_targets[j], decoded_preds[j]))
+                    test_wer.append(words.wer(decoded_targets[j], decoded_preds[j]))
+
+    avg_cer = sum(test_cer) / len(test_cer)
+    avg_wer = sum(test_wer) / len(test_wer)
+    experiment.log_metric("test_loss", test_loss, step=iter_meter.get())
+    experiment.log_metric("cer", avg_cer, step=iter_meter.get())
+    experiment.log_metric("wer", avg_wer, step=iter_meter.get())
+
+    print(
+        "Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n".format(
+            test_loss, avg_cer, avg_wer
+        )
+    )
+
+
+def GreedyDecoder(
+    output, labels, label_lengths, blank_label=28, collapse_repeated=True
+):
+    arg_maxes = torch.argmax(output, dim=2)
+    decodes = []
+    targets = []
+    for i, args in enumerate(arg_maxes):
+        decode = []
+        targets.append(
+            data.text_transform.int_to_text(labels[i][: label_lengths[i]].tolist())
+        )
+        for j, index in enumerate(args):
+            if index != blank_label:
+                if collapse_repeated and j != 0 and index == args[j - 1]:
+                    continue
+                decode.append(index.item())
+        decodes.append(data.text_transform.int_to_text(decode))
+    return decodes, targets
+
+
+def main(experiment):
+    hparams = {
+        "batch_size": 32,
+        "epochs": 2,
+        "learning_rate": 5e-4,
+        "n_cnn_layers": 3,
+        "n_rnn_layers": 5,
+        "rnn_dim": 512,
+        "n_class": 29,
+        "n_feats": 80,
+        "stride": 2,
+        "dropout": 0.1,
+    }
+    experiment.log_parameters(hparams)
+
+    torch.manual_seed(7)
+
+    test_dataset = torchaudio.datasets.LIBRISPEECH("/data", url="dev-clean")
+    train_clean_100 = torchaudio.datasets.LIBRISPEECH("/data", url="train-clean-100")
+    train_clean_360 = torchaudio.datasets.LIBRISPEECH("/data", url="train-clean-360")
+    train_other_500 = torchaudio.datasets.LIBRISPEECH("/data", url="train-other-500")
+    train_dataset = torch.utils.data.ConcatDataset(
+        [train_clean_100, train_clean_360, train_other_500]
+    )
+
+    kwargs = {"num_workers": 4, "pin_memory": True}
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=True,
+        collate_fn=lambda x: data.data_processing(x, "train"),
+        **kwargs
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=False,
+        collate_fn=lambda x: data.data_processing(x, "valid"),
+        **kwargs
+    )
+
+    model = net.SpeechRecognitionModel(
+        hparams["n_cnn_layers"],
+        hparams["n_rnn_layers"],
+        hparams["rnn_dim"],
+        hparams["n_class"],
+        hparams["n_feats"],
+        hparams["stride"],
+        hparams["dropout"],
+    )
+    model = nn.DataParallel(model)
+    model.cuda()
+
+    # print(model)
+    print(
+        "Num Model Parameters", sum([param.nelement() for param in model.parameters()])
+    )
+
+    optimizer = torch.optim.AdamW(model.parameters(), hparams["learning_rate"])
+    criterion = nn.CTCLoss(blank=28).cuda()
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=hparams["learning_rate"],
+        steps_per_epoch=int(len(train_loader)),
+        epochs=hparams["epochs"],
+        anneal_strategy="linear",
+    )
+
+    iter_meter = IterMeter()
+    for epoch in range(1, hparams["epochs"] + 1):
+        train(
+            model,
+            train_loader,
+            criterion,
+            optimizer,
+            scheduler,
+            epoch,
+            iter_meter,
+            experiment,
+        )
+        test(model, test_loader, criterion, epoch, iter_meter, experiment)
+
+
+if __name__ == "__main__":
+    experiment = Experiment(
+        api_key="IJIo1bzzY2MAGvPlhq9hA7qsb",
+        project_name="general",
+        workspace="fernand",
+        # disabled=True,
+    )
+    main(experiment)
