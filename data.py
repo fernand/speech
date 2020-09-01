Fixed testing, adding normalization and power=2

data.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 
 import numpy as np
 import torch
@@ -10,14 +11,14 @@ from text import TextTransform
 
 train_audio_transforms = nn.Sequential(
     torchaudio.transforms.MelSpectrogram(
-        sample_rate=16000, n_fft=400, hop_length=160, n_mels=80, power=1.0
+        sample_rate=16000, n_fft=400, hop_length=160, n_mels=80
     ),
     # torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
     # torchaudio.transforms.TimeMasking(time_mask_param=35),
 )
 
 valid_audio_transforms = torchaudio.transforms.MelSpectrogram(
-    sample_rate=16000, n_fft=400, hop_length=160, n_mels=80, power=1.0
+    sample_rate=16000, n_fft=400, hop_length=160, n_mels=80
 )
 
 text_transform = TextTransform()
@@ -33,6 +34,8 @@ class SortedTrainLibriSpeech(torch.utils.data.Dataset):
         if "train" in dataset_path:
             # Remove the longest and shortest clips.
             self.paths = self.paths[17000:][:-1000]
+        else:
+            random.shuffle(self.paths)
 
     def __len__(self):
         return len(self.paths) // self.batch_size
@@ -52,7 +55,7 @@ class SortedTrainLibriSpeech(torch.utils.data.Dataset):
         file_text = speaker_id + "-" + chapter_id + ".trans.txt"
         file_text = os.path.join(path, file_text)
 
-        waveform, sample_rate = torchaudio.load(audio_path)
+        waveform, sample_rate = torchaudio.load(audio_path, normalization=True)
 
         with open(file_text) as f:
             for line in f:

train.py
@@ -173,9 +173,11 @@ def main(hparams, experiment):
     torch.manual_seed(7)
 
     test_dataset = data.SortedTrainLibriSpeech(
-        "sorted_dev_clean_librispeech.pkl", 6
+        "sorted_dev_clean_librispeech.pkl", hparams["batch_size"]
     )
-    train_dataset = data.SortedTrainLibriSpeech("sorted_train_librispeech.pkl", hparams["batch_size"])
+    train_dataset = data.SortedTrainLibriSpeech(
+        "sorted_train_librispeech.pkl", hparams["batch_size"]
+    )
 
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
@@ -188,6 +190,7 @@ def main(hparams, experiment):
     test_loader = torch.utils.data.DataLoader(
         dataset=test_dataset,
         batch_size=None,
+        # Also shuffling at the clip level in data.py
         shuffle=True,
         collate_fn=lambda x: data.collate_fn(x, "valid"),
         num_workers=2,
@@ -224,7 +227,15 @@ def main(hparams, experiment):
             iter_meter,
             experiment,
         )
-        test(hparams["batch_size"], model, test_loader, criterion, epoch, iter_meter, experiment)
+        test(
+            hparams["batch_size"],
+            model,
+            test_loader,
+            criterion,
+            epoch,
+            iter_meter,
+            experiment,
+        )
 
 
 if __name__ == "__main__":
@@ -240,8 +251,7 @@ if __name__ == "__main__":
         # "batch_size": 10,
         "batch_size": 20,
         "epochs": 3,
-        #"learning_rate": 2.5e-3,
-        "learning_rate": 1e-3,
+        "learning_rate": 2.5e-3,
         # Does not include the blank.
         "n_vocab": 28,
         "n_feats": 80,