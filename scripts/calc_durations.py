1cf6be9260afb9da3348cca88a18b234292c061e
Fernand Pajot <fernand@epigram.me>
2020-11-14 15:09:56 -0700

    Master port.

--- scripts/calc_durations.py
+++ scripts/calc_durations.py
@@ -1,9 +1,12 @@
+import csv
 import multiprocessing
 import os
 import pickle
 
 import torchaudio
 from torchaudio.datasets.utils import walk_files
+
+torchaudio.set_audio_backend("sox_io")
 
 
 def get_librispeech_audio_files(prefix):
@@ -21,17 +24,34 @@ def get_librispeech_audio_files(prefix):
     return audio_files
 
 
+def get_common_voice_audio_files(prefix):
+    datasets = [
+        "train.tsv",
+        # "dev.tsv",
+        # "test.tsv",
+    ]
+    dataset_prefix = os.path.join(prefix, "cv-corpus-5.1-2020-06-22/en")
+    dataset_paths = [os.path.join(dataset_prefix, d) for d in datasets]
+    audio_files = []
+    for path in dataset_paths:
+        with open(path, newline="") as tsv:
+            reader = csv.DictReader(tsv, delimiter="\t")
+            for row in reader:
+                audio_files.append(os.path.join(dataset_prefix, "clips", row["path"]))
+    return audio_files
+
+
 def get_duration(path):
     y, sample_rate = torchaudio.load(path)
-    assert sample_rate == 16000
-    return y.size(1)
+    return y.size(1) / sample_rate
 
 
 if __name__ == "__main__":
     p = multiprocessing.Pool(6)
-    audio_files = get_librispeech_audio_files("/data")
+    # audio_files = get_librispeech_audio_files("/data")
+    audio_files = get_common_voice_audio_files("/data")
     durations = p.map(get_duration, audio_files)
     sorted_durations = sorted(zip(audio_files, durations), key=lambda t: t[1])
     # with open("sorted_train_librispeech.pkl", "wb") as f:
-    with open("sorted_dev_clean_librispeech.pkl", "wb") as f:
+    with open("sorted_train_commonvoice.pkl", "wb") as f:
         pickle.dump(sorted_durations, f)
