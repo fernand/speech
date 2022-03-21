e72be269787ac3434d3a2323528ab3fe69930020
Fernand Pajot <fernand@epigram.me>
2022-03-20 18:30:40 -0600

    WIP

--- process/process_audio.py
+++ process/process_audio.py
@@ -1,21 +1,18 @@
 import itertools
+import math
 import multiprocessing
 import os
-import pickle
 import re
 import random
 import shutil
 import subprocess
 import sys
-import tempfile
 import time
 import uuid
 
-import aeneas.task
-import aeneas.executetask
-import scipy.io.wavfile
-import sox
 import numpy as np
+import scipy.io.wavfile
+import torch
 
 import int_to_words
 
@@ -118,19 +115,21 @@ def parse_srt(srt_f):
     return transcripts
 
 
+# Split the transcript into 20 second chunks with one transcript overlap.
 def get_transcript_chunks(transcripts):
     chunks = []
     first_tr = transcripts[0]
     current_chunk = [first_tr]
-    prev_end = first_tr[1]
+    current_chunk_start = first_tr[0]
     for tr in transcripts[1:]:
-        current_start = tr[0]
-        if current_start - prev_end < 0.5:
+        current_end = tr[0]
+        if current_end - current_chunk_start < 20:
             current_chunk.append(tr)
         else:
             chunks.append(current_chunk)
-            current_chunk = [tr]
-        prev_end = tr[1]
+            first_tr = current_chunk[-1]
+            current_chunk = [first_tr, tr]
+            current_chunk_start = first_tr[0]
     chunks.append(current_chunk)
     return chunks
 
@@ -159,52 +158,21 @@ def ac3_to_wav(audio_f, output_dir):
 
 def split_audio_to_chunks(audio_f, transcript_chunks, output_dir):
     sr, y = scipy.io.wavfile.read(audio_f)
+    len_y = len(y)
     assert sr == 16000
     outputs = []
     for i, chunk in enumerate(transcript_chunks):
         if len(chunk) <= 3:
             continue
-        start, end = chunk[0][0], chunk[-1][1]
-        tfm = sox.Transformer()
-        tfm.trim(start, end)
+        start_s, end_s = chunk[0][0], chunk[-1][1]
+        start = max(0, int(sr * start_s))
+        end = min(len_y, math.ceil(sr * end_s))
         output_f = os.path.join(
             output_dir, os.path.basename(audio_f).split(".")[0] + f"_{i}.wav"
         )
-        tfm.build_file(input_array=y, sample_rate_in=sr, output_filepath=output_f)
+        scipy.io.wavfile.write(output_f, sr, y[start:end])
         outputs.append((output_f, "\n".join([tr[2] for tr in chunk])))
     return outputs
-
-
-def align_audio(audio_f, transcript_lines):
-    tmp_txt = tempfile.NamedTemporaryFile(delete=False)
-    tmp_txt.write(transcript_lines.encode("utf-8"))
-    tmp_txt.close()
-    config = "task_language=en|is_text_type=plain|os_task_file_format=txt"
-    task = aeneas.task.Task(config_string=config)
-    task.audio_file_path_absolute = audio_f
-    task.text_file_path_absolute = tmp_txt.name
-    aeneas.executetask.ExecuteTask(task).execute()
-    os.remove(tmp_txt.name)
-    return task.sync_map
-
-
-def split_chunk_to_uterances(audio_f, fragments, output_dir):
-    sr, y = scipy.io.wavfile.read(audio_f)
-    assert sr == 16000
-    for i, fragment in enumerate(fragments):
-        start, end = float(str(fragment.begin)), float(str(fragment.end))
-        # Ignore utterances shorter than 1 second
-        if end < start + 1.0:
-            continue
-        tfm = sox.Transformer()
-        tfm.trim(start, end)
-        output_f = os.path.join(
-            output_dir, os.path.basename(audio_f).split(".")[0] + f"_{i}.wav"
-        )
-        tfm.build_file(input_array=y, sample_rate_in=sr, output_filepath=output_f)
-        output_txt = output_f.strip(".wav") + ".txt"
-        with open(output_txt, "w") as txt_f:
-            txt_f.write(fragment.text + "\n")
 
 
 def opusenc(audio_f):
@@ -215,7 +183,7 @@ def opusenc(audio_f):
     os.remove(audio_f)
 
 
-def process_file(audio_f, output_dir):
+def process_file(audio_f, output_dir, model, decoder, model_utils):
     srt_f = audio_f.split(".")[0] + ".srt"
     transcripts = parse_srt(srt_f)
     if len(transcripts) == 0:
@@ -226,31 +194,57 @@ def process_file(audio_f, output_dir):
     chunks = get_transcript_chunks(transcripts)
     outputs = split_audio_to_chunks(wav_f, chunks, output_dir)
     os.remove(wav_f)
-    for audio_chunk_f, transcript in outputs:
-        # Make sure the wavfile is valid.
-        sr, y = scipy.io.wavfile.read(audio_chunk_f)
-        if len(y) == 0:
-            os.remove(audio_chunk_f)
-            continue
-        sync_map = align_audio(audio_chunk_f, transcript)
-        fragments = sync_map.leaves(fragment_type=0)
-        split_chunk_to_uterances(audio_chunk_f, fragments, output_dir)
-        os.remove(audio_chunk_f)
+
+    audio_chunk_files = [o[0] for o in outputs]
+    (read_batch, split_into_batches, read_audio, prepare_model_input) = model_utils
+    batches = split_into_batches(audio_chunk_files, batch_size=32)
+
+    silero_alignments = []
+    file_idx = 0
+    for batch in batches:
+        loaded_batch = read_batch(batch)
+        input = prepare_model_input(loaded_batch, device=device)
+        output = model(input)
+        for i, example in enumerate(output):
+            alignment = decoder(
+                example.cpu(), wav_len=len(loaded_batch[i]), word_align=True
+            )
+            print(batch[i], outputs[file_idx][1].replace("\n", " "))
+            print(alignment)
+            print("------------")
+            silero_alignments.append(alignment)
+            file_idx += 1
+    print(audio_f)
+    print("=====================================")
+
+    # for i, t in enumerate(outputs):
+    #     audio_chunk_f, transcripts = t
+    #     alignment = silero_alignments[i]
+
+    #     # Make sure the wavfile is valid.
+    #     sr, y = scipy.io.wavfile.read(audio_chunk_f)
+    #     if len(y) == 0:
+    #         os.remove(audio_chunk_f)
+    #         continue
 
 
 DATASETS = {
     "first": {
-        "input_dirs": ["/tv/first", "/tv/first/extra", "/tv/first/round1"],
-        "output_dir": "/hd1/clean",
+        "input_dirs": ["/backup/first", "/backup/first/extra", "/backup/first/round1"],
+        "output_dir": "/nvme/clean",
     },
     "second": {
-        "input_dirs": ["/tv/second", "/tv/second/first", "/tv/second/second"],
-        "output_dir": "/hd1/clean2",
+        "input_dirs": [
+            "/backup/second",
+            "/backup/second/first",
+            "/backup/second/second",
+        ],
+        "output_dir": "/nvme/clean2",
     },
-    "third": {"input_dirs": ["/tv/third"], "output_dir": "/hd1/clean3"},
-    "fourth": {"input_dirs": ["/tv/fourth"], "output_dir": "/hd1/clean4"},
-    "fifth": {"input_dirs": ["/tv/fifth"], "output_dir": "/hd1/clean5"},
-    "sixth": {"input_dirs": ["/tv/sixth"], "output_dir": "/hd1/clean6"},
+    "third": {"input_dirs": ["/backup/third"], "output_dir": "/nvme/clean3"},
+    "fourth": {"input_dirs": ["/backup/fourth"], "output_dir": "/nvme/clean4"},
+    "fifth": {"input_dirs": ["/backup/fifth"], "output_dir": "/nvme/clean5"},
+    "sixth": {"input_dirs": ["/backup/sixth"], "output_dir": "/nvme/clean6"},
 }
 
 if __name__ == "__main__":
@@ -263,14 +257,29 @@ if __name__ == "__main__":
     audio_files = list_input_audio_files(input_dirs)
     random.shuffle(audio_files)
     print(f"{len(audio_files)} files to process")
+
+    device = torch.device("cuda:0")
+    model, decoder, model_utils = torch.hub.load(
+        repo_or_dir="snakers4/silero-models",
+        model="silero_stt",
+        language="en",
+        device=device,
+    )
+
+    # process_file("/backup/first/1_4_228.ac3", output_dir, model, decoder, model_utils)
+    process_file(audio_files[0], output_dir, model, decoder, model_utils)
+
     # Process by chunks in order to not run into RAM issues.
-    num_chunks = 20
-    chunks = np.array_split(audio_files, num_chunks)
-    for chunk_i, chunk in enumerate(chunks):
-        p = multiprocessing.Pool(32)
-        print(f"Processing chunk {chunk_i} out of {num_chunks - 1}")
-        start = time.time()
-        p.starmap(process_file, [(audio_f, output_dir) for audio_f in chunks[chunk_i]])
-        print(f"Time to process: {round(time.time() - start)}s")
-        p.close()
-        p.join()
+    # num_chunks = 20
+    # chunks = np.array_split(audio_files, num_chunks)
+    # for chunk_i, chunk in enumerate(chunks):
+    #     p = multiprocessing.Pool(32)
+    #     print(f"Processing chunk {chunk_i} out of {num_chunks - 1}")
+    #     start = time.time()
+    #     p.starmap(process_file, [(audio_f, output_dir) for audio_f in chunks[chunk_i]])
+    #     print(f"Time to process: {round(time.time() - start)}s")
+    #     p.close()
+    #     p.join()
+
+a = "mary jenkins  madame you're a genius this work captures the frustration of the modern housewife  mary is he serious  seriously ill what else have you created  i created a daughter  but i had some help  i'm sorry i have to s ay i'm not an artist"
+b = "deins mea you're a genius this work captures the frustgrace of the modern housewife mary is he serious seriously ill what have you created well i created a donllar but i had little i'm sorryi have to tell you i'm not in"
